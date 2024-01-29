58dd49af899b86994e4d9ade7015adb384540a29
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-01-29 14:50:04 -0700

    Add CSS classes for different product states

--- assets/product.js
+++ assets/product.js
@@ -2,3 +2,9 @@
 const addToCart = document.querySelector('form[data-form="addItem"]');
 
 console.log(addToCart);
+
+// In Stock: "cursor-pointer",
+// Out of Stock: "opacity-25 cursor-not-allowed"
+// Active: "ring-2 ring-indigo-500 ring-offset-2"
+// Checked: "border-transparent bg-indigo-600 text-white hover:bg-indigo-700",
+// Not Checked: "border-gray-200 bg-white text-gray-900 hover:bg-gray-50"
